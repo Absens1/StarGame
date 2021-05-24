652823f177614aacb451c6642c07215760c97dd8
Daniil <ha.li.fa@yandex.ru>
2021-05-24 14:27:36 +0300

    Initial commit

---
+++ desktop/src/ru/abs/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ru.abs.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ru.abs.StarGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new StarGame(), config);
+	}
+}
