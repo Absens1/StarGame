64ebd4e1245db0bad45f749b45b8fa49874cc77c
Daniil <ha.li.fa@yandex.ru>
2021-06-03 13:22:09 +0300

    hw3 - default realization

--- core/src/ru/abs/StarGame.java
+++ core/src/ru/abs/StarGame.java
@@ -5,8 +5,10 @@ import com.badlogic.gdx.Game;
 import ru.abs.screen.MenuScreen;
 
 public class StarGame extends Game {
-	@Override
-	public void create() {
-		setScreen(new MenuScreen());
-	}
+
+    @Override
+    public void create() {
+    	setScreen(new MenuScreen());
+    }
+
 }
--- core/src/ru/abs/screen/MenuScreen.java
+++ core/src/ru/abs/screen/MenuScreen.java
@@ -1,29 +1,30 @@
 package ru.abs.screen;
 
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 import ru.abs.base.BaseScreen;
+import ru.abs.math.Rect;
+import ru.abs.sprite.Background;
 
 public class MenuScreen extends BaseScreen {
 
-    private Texture img;
-    private Vector2 pos;
-    private Vector2 v;
-    private Vector2 movePosition;
-    private Vector2 tmp;
-    private static final float LEN = 3F;
+    private Texture bg;
+
+    private Background background;
 
     @Override
     public void show() {
         super.show();
-        img = new Texture("badlogic.jpg");
-        pos = new Vector2();
-        movePosition = new Vector2();
-        v = new Vector2();
-        tmp = new Vector2();
+        bg = new Texture("textures/bg.png");
+        background = new Background(bg);
+    }
+
+    @Override
+    public void resize(Rect worldBounds) {
+        super.resize(worldBounds);
+        background.resize(worldBounds);
     }
 
     @Override
@@ -31,27 +32,18 @@ public class MenuScreen extends BaseScreen {
         super.render(delta);
         ScreenUtils.clear(0.33f, 0.45f, 0.68f, 1);
         batch.begin();
-        batch.draw(img, pos.x, pos.y);
+        background.draw(batch);
         batch.end();
-        tmp.set(movePosition);
-        if (tmp.sub(pos).len() <= v.len()) {
-            pos.set(movePosition);
-            v.setZero();
-        } else {
-            pos.add(v);
-        }
     }
 
     @Override
     public void dispose() {
         super.dispose();
-        img.dispose();
+        bg.dispose();
     }
 
     @Override
-    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-        movePosition.set(screenX, Gdx.graphics.getHeight() - screenY);
-        v.set(movePosition.cpy().sub(pos)).setLength(LEN);
+    public boolean touchDown(Vector2 touch, int pointer, int button) {
         return false;
     }
 }
