bb246c214d0bca0c4b2ed568c76c33dface5b80a
Daniil <ha.li.fa@yandex.ru>
2021-06-03 13:26:25 +0300

    hw3 - adapt hw2 to the new architecture

--- core/src/ru/abs/screen/MenuScreen.java
+++ core/src/ru/abs/screen/MenuScreen.java
@@ -1,5 +1,4 @@
 package ru.abs.screen;
-
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.ScreenUtils;
@@ -7,32 +6,40 @@ import com.badlogic.gdx.utils.ScreenUtils;
 import ru.abs.base.BaseScreen;
 import ru.abs.math.Rect;
 import ru.abs.sprite.Background;
+import ru.abs.sprite.Logo;
 
 public class MenuScreen extends BaseScreen {
 
     private Texture bg;
+    private Texture badlogic;
 
     private Background background;
+    private Logo logo;
 
     @Override
     public void show() {
         super.show();
         bg = new Texture("textures/bg.png");
         background = new Background(bg);
+        badlogic = new Texture("badlogic.jpg");
+        logo = new Logo(badlogic);
     }
 
     @Override
     public void resize(Rect worldBounds) {
         super.resize(worldBounds);
         background.resize(worldBounds);
+        logo.resize(worldBounds);
     }
 
     @Override
     public void render(float delta) {
         super.render(delta);
+        logo.update(delta);
         ScreenUtils.clear(0.33f, 0.45f, 0.68f, 1);
         batch.begin();
         background.draw(batch);
+        logo.draw(batch);
         batch.end();
     }
 
@@ -40,10 +47,12 @@ public class MenuScreen extends BaseScreen {
     public void dispose() {
         super.dispose();
         bg.dispose();
+        badlogic.dispose();
     }
 
     @Override
     public boolean touchDown(Vector2 touch, int pointer, int button) {
+        logo.touchDown(touch, pointer, button);
         return false;
     }
 }
